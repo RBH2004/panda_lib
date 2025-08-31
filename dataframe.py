63f3595915e4d0b9d015c381158222ae3817b1ff
RBH2004 <hassanrafidbin@gmail.com>
2025-08-31 21:20:38 +0600

    Create dataframe.py

---
+++ dataframe.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+l=[1,2,3,4,5,6]
+
+var=pd.DataFrame(l)
+print(var)
+
+#   0
+#0  1
+#1  2
+#2  3
+#3  4
+#4  5
+#5  6
+
+print(type(var))
+
+#<class 'pandas.core.frame.DataFrame'>
+
+
+dic={'a':[1,2,3,4],'s':[5,6,7,8]}
+var2=pd.DataFrame(dic)
+print(var2)
+
+#   a  s
+#0  1  5
+#1  2  6
+#2  3  7
+#3  4  8
+
+var3=pd.DataFrame(dic,columns=['a'])
+print(var3)
+#only those colums we need for the data
+#   a
+#0  1
+#1  2
+#2  3
+#3  4
