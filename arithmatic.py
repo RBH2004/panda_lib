41fc4cda485175cc4134a5a961c7b7381c3f6179
RBH2004 <hassanrafidbin@gmail.com>
2025-09-01 20:57:58 +0600

    Update arithmatic.py

--- arithmatic.py
+++ arithmatic.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 arr=pd.DataFrame({"A":[1,2,3,4],"B":[5,6,7,8]})
 
-arr["C"]=arr["A"]+arr["B"]
+arr["C"]=arr["A"]+arr["B"] #anything op="+,-,*,/"
 
 print(arr)
 #   A  B   C
@@ -10,3 +10,12 @@ print(arr)
 #1  2  6   8
 #2  3  7  10
 #3  4  8  12
+
+arr['checker']=arr['A']<=3
+print(arr)
+
+#   A  B   C  checker
+#0  1  5   6     True
+#1  2  6   8     True
+#2  3  7  10     True
+#3  4  8  12    False
