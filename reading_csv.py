a458351e4ef3eb4a38cd5c1e30de4266a7d205ef
RBH2004 <hassanrafidbin@gmail.com>
2025-09-02 21:48:16 +0600

    .

--- reading_csv.py
+++ reading_csv.py
@@ -10,4 +10,22 @@ print(row_one)
 #
 #[1 rows x 15 columns]
 
-get_column
+get_column=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv",usecols=['pickup_year'])
+print(get_column)
+get_column_2=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv",usecols=[0,1])
+print(get_column_2)
+
+
+#[1 rows x 15 columns]
+#       pickup_year
+#0             2016
+#1             2016
+#2             2016
+#3             2016
+#4             2016
+#...            ...
+#89555         2016
+#89556         2016
+#89557         2016
+#89558         2016
+#89559         2016
