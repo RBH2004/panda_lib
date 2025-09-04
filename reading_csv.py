2f8d73a36842baa741e29f4cc2f3d8e885014ecf
RBH2004 <hassanrafidbin@gmail.com>
2025-09-04 22:45:58 +0600

    .

--- reading_csv.py
+++ reading_csv.py
@@ -106,42 +106,33 @@ print(header_build)
 #[89558 rows x 15 columns]
 
 
-name_heading=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv",names=['Unga bunga'])
+name_heading=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv",names=['Unga bunga','nigga'])
 print(name_heading)
 
 
-                                                                                                  
-#  Unga bunga
-#pickup_year pickup_month pickup_day pickup_dayofweek pickup_time pickup_location_code dropoff_location_code trip_distance trip_length fare_amount fees_amount tolls_amount tip_amount total_amount  payment_type
+#                                                                                                  
+#                                                                                       Unga bunga         nigga
+#pickup_year pickup_month pickup_day pickup_dayofweek pickup_time pickup_location_code dropoff_location_code trip_distance trip_length fare_amount fees_amount tolls_amount tip_amount  total_amount  payment_type
 #2016        1            1          5                0           2                    4           
-#          21.00         2037        52.00       0.80        5.54         11.65      69.99         
-#           1
+#          21.00         2037        52.00       0.80        5.54         11.65              69.99             1
 #                                                                                      1           
-#          16.29         1520        45.00       1.30        0.00         8.00       54.30         
-#           1
+#          16.29         1520        45.00       1.30        0.00         8.00               54.30             1
 #                                                                                      6           
-#          12.70         1462        36.50       1.30        0.00         0.00       37.80         
-#           2
+#          12.70         1462        36.50       1.30        0.00         0.00               37.80             2
 #                                                                                                  
-#          8.70          1210        26.00       1.30        0.00         5.46       32.76         
-#           1
+#          8.70          1210        26.00       1.30        0.00         5.46               32.76             1
 #...                                                                                               
-#                                                                                                  
-#         ...
+#                                                                                              ...           ...
 #            6            30         4                5           3                    4           
-#          9.5           1989        31.0        1.3         5.54         3.0        40.84         
-#           1
+#          9.5           1989        31.0        1.3         5.54         3.0                40.84             1
 #                                                                 2                    4           
-#          19.8          2368        52.0        0.8         5.54         0.0        58.34         
-#           1
+#          19.8          2368        52.0        0.8         5.54         0.0                58.34             1
 #                                                                                                  
-#          17.48         2822        52.0        0.8         5.54         5.0        63.34         
-#           1
+#          17.48         2822        52.0        0.8         5.54         5.0                63.34             1
 #                                                                                      6           
-#          12.76         1083        34.5        1.3         0.0          8.95       44.75                    1
-#                                                                                      0                     17.54         1711        48.0        1.3         5.54         0.0        54.84                    2
-#
-#[89561 rows x 1 columns]
+#          12.76         1083        34.5        1.3         0.0          8.95               44.75             1
+#                                                                                      0           
+#          17.54         1711        48.0        1.3         5.54         0.0                54.84             2
 
 
 deleted_header=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv",header=None)
---
+++ writing_csv.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+data=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv")
+
