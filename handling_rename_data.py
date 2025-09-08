46794641644cea73a5557c6ad6c46cba85dae8c6
RBH2004 <hassanrafidbin@gmail.com>
2025-09-08 21:28:37 +0600

    Update handling_rename_data.py

--- handling_rename_data.py
+++ handling_rename_data.py
@@ -8,7 +8,7 @@ data=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv")
 data.loc[0,'pickup_year']=None
 data.loc[0,'pickup_month']=None
 
-print(data)
+#print(data)
 
 #       pickup_year  pickup_month  pickup_day  ...  tip_amount  total_amount  payment_type
 #0              NaN           NaN           1  ...       11.65         69.99             1
@@ -26,8 +26,8 @@ print(data)
 #[89560 rows x 15 columns]
 
 
-data=data.dropna()
-print(data)
+data2=data.dropna()
+print(data2)
 
 
 
@@ -47,3 +47,21 @@ print(data)
 #[89559 rows x 15 columns]
 
 
+data3=data.dropna(axis=1) #removes columns with Nan data. similarly axis=0 removes rows with Nan data
+print(data3)
+#
+#       pickup_day  pickup_dayofweek  pickup_time  ...  tip_amount  total_amount  payment_type
+#0               1                 5            0  ...       11.65         69.99             1     
+#1               1                 5            0  ...        8.00         54.30             1     
+#2               1                 5            0  ...        0.00         37.80             2     
+#3               1                 5            0  ...        5.46         32.76             1     
+#4               1                 5            0  ...        0.00         18.80             2     
+#...           ...               ...          ...  ...         ...           ...           ...     
+#89555          30                 4            5  ...        3.00         40.84             1     
+#89556          30                 4            5  ...        0.00         58.34             1     
+#89557          30                 4            5  ...        5.00         63.34             1     
+#89558          30                 4            5  ...        8.95         44.75             1     
+#89559          30                 4            5  ...        0.00         54.84             2     
+#
+#[89560 rows x 13 columns]
+#
