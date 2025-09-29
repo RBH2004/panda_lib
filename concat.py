c35a39eb90939e9b41c24c5d54cbffc79903a302
RBH2004 <hassanrafidbin@gmail.com>
2025-09-29 16:18:19 +0600

    Update concat.py

--- concat.py
+++ concat.py
@@ -33,3 +33,42 @@ print(data)
 #1  2  22
 #2  3  23
 #3  5  24
+
+
+data=pd.concat([arr1,arr2],axis=1)
+print(data)
+
+#   A   B  A   B
+#0  1  11  1  21
+#1  2  12  2  22
+#2  3  13  3  23
+#3  4  14  5  24
+
+
+
+arr1=pd.DataFrame({'A':[1,2,3,4],'B':[11,12,13,14]})
+arr2=pd.DataFrame({'A':[1,2],'B':[21,22]})
+
+data=pd.concat([arr1,arr2],axis=1)
+print(data)
+
+#   A   B    A     B
+#0  1  11  1.0  21.0
+#1  2  12  2.0  22.0
+#2  3  13  NaN   NaN
+#3  4  14  NaN   NaN
+
+data=pd.concat([arr1,arr2],axis=1,join="outer")
+print(data)
+#   A   B    A     B
+#0  1  11  1.0  21.0
+#1  2  12  2.0  22.0
+#2  3  13  NaN   NaN
+#3  4  14  NaN   NaN
+
+data=pd.concat([arr1,arr2],axis=1,join='inner')
+print(data)
+
+#   A   B  A   B
+#0  1  11  1  21
+#1  2  12  2  22
