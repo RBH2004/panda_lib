eb4649fac16ab26a0bd227fa87300cfc9d6326fb
RBH2004 <hassanrafidbin@gmail.com>
2025-09-29 14:13:11 +0600

    Create interpolate.py

---
+++ interpolate.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+data=pd.read_csv("D:\\github\\panda\\nyc_taxis.csv")
+
+data.loc[0,'pickup_year']=None
+data.loc[1,'pickup_year']=None
+data.loc[2,'pickup_year']=None
+data.loc[0,'pickup_month']=None
+
+print(data)
+
+#       pickup_year  pickup_month  pickup_day  ...  tip_amount  total_amount  payment_type
+#0              NaN           NaN           1  ...       11.65         69.99             1
+#1              NaN           1.0           1  ...        8.00         54.30             1
+#2              NaN           1.0           1  ...        0.00         37.80             2
+#3           2016.0           1.0           1  ...        5.46         32.76             1
+#4           2016.0           1.0           1  ...        0.00         18.80             2
+#...            ...           ...         ...  ...         ...           ...           ...
+#89555       2016.0           6.0          30  ...        3.00         40.84             1
+#89556       2016.0           6.0          30  ...        0.00         58.34             1
+#89557       2016.0           6.0          30  ...        5.00         63.34             1
+#89558       2016.0           6.0          30  ...        8.95         44.75             1
+#89559       2016.0           6.0          30  ...        0.00         54.84             2
+#
+#[89560 rows x 15 columns]
