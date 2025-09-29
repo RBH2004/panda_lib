9fae07c4302d9f79b2ddd154e7e3b18b1ff203d6
RBH2004 <hassanrafidbin@gmail.com>
2025-09-29 15:17:06 +0600

    Update merge-dataframe.py

--- merge-dataframe.py
+++ merge-dataframe.py
@@ -10,3 +10,42 @@ print(data)
 #0  1  11  21
 #1  2  12  22
 #2  3  13  23
+
+data2=pd.merge(arr1,arr2,how='inner')
+print(data2)
+
+#   A   B   C
+#0  1  11  21
+#1  2  12  22
+#2  3  13  23
+
+data2=pd.merge(arr1,arr2,how='left')
+print(data2)
+
+#   A   B     C
+#0  1  11  21.0
+#1  2  12  22.0
+#2  3  13  23.0
+#3  4  14   NaN
+
+data3=pd.merge(arr1,arr2,how='right')
+print(data3)
+
+#   A     B   C
+#0  1  11.0  21
+#1  2  12.0  22
+#2  3  13.0  23
+#3  5   NaN  24
+
+data4=pd.merge(arr1,arr2,how='outer')
+print(data4)
+
+#   A     B     C
+#0  1  11.0  21.0
+#1  2  12.0  22.0
+#2  3  13.0  23.0
+#3  4  14.0   NaN
+#4  5   NaN  24.0
+
+
+
