51c0e8dc55308bda1c228759b886fdcb6f0c5e50
RBH2004 <hassanrafidbin@gmail.com>
2025-09-29 22:04:32 +0600

    Update join.py

--- join.py
+++ join.py
@@ -102,7 +102,27 @@ print(data)
 
 
 arr1=pd.DataFrame({'A':[1,2,3,4],"B":[11,12,13,14]},index=['a','b','c','d'])
-arr2=pd.DataFrame({"C":[21,22],"D":[31,32]},index=['a','b'])
+arr2=pd.DataFrame({"C":[21,22],"B":[31,32]},index=['a','b'])
 
-data=arr1.join(arr2,how="right")
+data=arr1.join(arr2,lsuffix='_12')
 print(data)
+
+
+#   A  B_12     C     B
+#a  1    11  21.0  31.0
+#b  2    12  22.0  32.0
+#c  3    13   NaN   NaN
+#d  4    14   NaN   NaN
+
+
+arr1=pd.DataFrame({'A':[1,2,3,4],"B":[11,12,13,14]},index=['a','b','c','d'])
+arr2=pd.DataFrame({"B":[21,22],"D":[31,32]},index=['a','b'])
+
+data=arr1.join(arr2,lsuffix='_12',rsuffix='_22')
+print(data)
+
+#   A  B_12  B_22     D
+#a  1    11  21.0  31.0
+#b  2    12  22.0  32.0
+#c  3    13   NaN   NaN
+#d  4    14   NaN   NaN
