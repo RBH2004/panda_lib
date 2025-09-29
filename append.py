c3b8533b938027fa2bb6da037577053463f7625d
RBH2004 <hassanrafidbin@gmail.com>
2025-09-29 22:25:29 +0600

    Update append.py

--- append.py
+++ append.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
 
+arr1=pd.DataFrame({'A':[1,2,3,4],"B":[11,12,13,14]},index=['a','b','c','d'])
+arr2=pd.DataFrame({"C":[21,22],"D":[31,32]},index=['a','b'])
+
+data=arr1.append(arr2)
+print(data)
